af3b648ee87ba58de000a46bef1608fc60c68b9b
UncouthClodhopper <96358734+UncouthClodhopper@users.noreply.github.com>
2023-01-12 11:28:47 -0600

    imported tmp
    imported tmp

---
+++ Assets/Scripts/MenuUIScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class MenuUIScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private textm
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
